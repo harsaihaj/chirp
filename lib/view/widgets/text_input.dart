6522f43b5f2e95ee7b47547e471f3f69995a3d5c
harsaihaj <harsaihaj.gill@gmail.com>
2025-01-04 17:51:45 +0530

    first commit

---
+++ lib/view/widgets/text_input.dart
@@ -0,0 +1,38 @@
+import 'package:chirp/constants.dart';
+import 'package:flutter/material.dart';
+
+class TextInputField extends StatelessWidget {
+  final TextEditingController controller;
+  final IconData myIcon;
+  final String myLabelText;
+  final bool toHide;
+
+
+  TextInputField(
+      {super.key,
+      required this.controller,
+      required this.myLabelText,
+      required this.myIcon,
+      this.toHide = false,
+      });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      obscureText: toHide,
+      controller: controller,
+      decoration: InputDecoration(
+        icon: Icon(myIcon),
+        labelText: myLabelText,
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: const BorderSide(color: borderColor),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(5),
+          borderSide: const BorderSide(color: borderColor),
+        ),
+      ),
+    );
+  }
+}
